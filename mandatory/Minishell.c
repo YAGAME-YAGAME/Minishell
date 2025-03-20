b64d63fa7f4da2232de014800434e8ed0891c010
AbdoCooder <abdocooder@gmail.com>
2025-03-20 05:21:42 +0000

    tree

--- mandatory/Minishell.c
+++ mandatory/Minishell.c
@@ -6,7 +6,7 @@
 /*   By: abenajib <abenajib@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 10:42:01 by abenajib          #+#    #+#             */
-/*   Updated: 2025/03/20 03:00:40 by abenajib         ###   ########.fr       */
+/*   Updated: 2025/03/20 05:21:38 by abenajib         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,36 +40,6 @@ void	ft_pipepos(t_list **current) // Working as expected
 	}
 }
 
-// t_parse *ft_tree(t_list **current)
-// {
-	// t_parse *root;
-	// t_parse *ptr;
-	// t_list *tmp;
-
-	// tmp = *current;
-	// root = ft_treenew(tmp);
-	// // printf("root [%s]\n", root->node->content);
-	// root->right = ft_treenew(tmp->next);
-	// // printf("root right [%s]\n", root->right->node->content);
-	// ptr = root->left;
-	// ft_pipepos(current);
-	// tmp = NULL;
-	// while (*current)
-	// {
-	// 	tmp = *current;
-	// 	ptr = ft_treenew(*current);
-	// 	// printf("ptr [%s]\n", ptr->node->content);
-	// 	ptr->right = ft_treenew((*current)->next);
-	// 	// printf("ptr right [%s]\n", ptr->right->node->content);
-	// 	ptr = ptr->left;
-	// 	ft_pipepos(current);
-	// 	tmp = NULL;
-	// }
-	// ptr = ft_treenew(lexer);
-	// return (root);
-
-// }
-
 // Add helper to check if node is a pipe
 int is_pipe(t_list *node) {
 	return (node && ft_strcmp(node->content, "|") == 0);
@@ -97,21 +67,16 @@ t_parse *ft_tree(t_list **current)
 	return node;
 }
 
+
 void	ft_print_tree(t_parse *root)
 {
 	if (!root)
-		return;
-	printf("Node: %s\n", root->node->content);
-	if (root->left)
 	{
-		printf("Left -> ");
-		ft_print_tree(root->left);
+		printf("(empty tree)\n");
+		return ;
 	}
-	if (root->right)
-	{
-		printf("Right -> ");
-		ft_print_tree(root->right);
-	}
+	printf("Parse Tree:\n");
+
 }
 
 t_parse	*ft_lst2tree(t_list *lexer)
@@ -127,8 +92,6 @@ t_parse	*ft_lst2tree(t_list *lexer)
 	else
 	{
 		root = ft_tree(&current);
-		ft_print_tree(root);
-		ft_print_tree(root->right);
 	}
 	return (root);
 }
@@ -145,8 +108,6 @@ void	ft_parse(t_minishell *minishell)
 
 	minishell->tree = ft_lst2tree(lstlexer);
 	// ft_print_tree(minishell->tree);
-	// ft_print_tree(minishell->tree->left);
-	// ft_print_tree(minishell->tree->right);
 
 	ft_lstclear(&lstlexer, free);
 
