ce6e90169ab67b9b44325b11765106e2d254f74e
YAGAME-YAGAME <otmane12low@gmail.com>
2025-05-17 16:02:45 +0100

    ..

--- minihell/builtins/cd.c
+++ minihell/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: yagame <yagame@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 01:12:04 by yagame            #+#    #+#             */
-/*   Updated: 2025/05/16 19:38:59 by yagame           ###   ########.fr       */
+/*   Updated: 2025/05/17 04:17:31 by yagame           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,7 +95,7 @@ int	ft_cd(char **cmd, t_list **env)
 	path = NULL;
 	old_path = ft_getenv("PWD", *env);
 	if (size_dp(cmd) > 2)
-		return (free(cmd), write(2, "minishell :cd: too many arguments\n", 34),
+		return (write(2, "minishell :cd: too many arguments\n", 34),
 			1);
 	if (cmd[1] == NULL || ft_strcmp(cmd[1], "~") == 0)
 	{
